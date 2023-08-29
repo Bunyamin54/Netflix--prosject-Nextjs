cb11e37fb61764bab7dbca8c1eb2eae848d1fb07
Bunyamin Genc <bunyaming@uia.no>
2023-08-29 22:03:31 +0200

    navbar

--- class-notes/nc-01/netflix-prosject/app/layout.js
+++ class-notes/nc-01/netflix-prosject/app/layout.js
@@ -1,17 +1,22 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
+//! metadata objesi ve generateMetadata fonksiyonu sadece Server Componentlerden export edilebilir.
 export const metadata = {
-  title: 'Netflix',
-  description: 'a movie platform',
-}
+  title: "Netflix",
+  description: "a movie platform",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
