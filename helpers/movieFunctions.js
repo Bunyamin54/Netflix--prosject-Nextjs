14f21f5459ca5caf9ee66bcec1ec845bb5e83e21
Bunyamin Genc <bunyaming@uia.no>
2023-08-31 21:00:42 +0200

    moviesections

--- helpers/movieFunctions.js
+++ helpers/movieFunctions.js
@@ -1,8 +1,12 @@
-const API_KEY = process.env.TMDB_KEY;
+const API_KEY = process.env.TMDB_KEY;c7c0ef4acac8943732a530a27fcd87a2
 
 export const getMovies = async (type) => {
   const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
   const res = await fetch(URL);
+  //? next.js ile fetch api çekilen verileri default olarak cache'ler. bu özelliği option objesi ile değiştirebiliriz
+  // const res = await fetch(URL, { cache: "force-cache" }); default
+  // const res = await fetch(URL, { cache: "no-store" }); cache'leme
+  //   const res = await fetch(URL, { next: { revalidate: 10 } }); belirli aralıklarla veriyi tekrar çek tekrar
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
