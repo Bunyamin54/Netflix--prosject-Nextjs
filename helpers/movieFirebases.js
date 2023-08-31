2eadad8717f4e9cb462e7ec055d33740629e8ab9
Bunyamin Genc <bunyaming@uia.no>
2023-08-31 20:30:28 +0200

    API KEY ENV

---
+++ helpers/movieFirebases.js
@@ -0,0 +1,7 @@
+const API_KEY = process.env.TMDB_KEY 
+
+export const getMovies = aync()=> {
+    const URL = `https://api.themoviedb.org/3/movie/${type}?api_key=${API_KEY}`;
+    const res =  await fetch(URL)
+
+}
